5dfde9185dd1b989970bdc7729b69543fd23aeb9
kyokid <havh0108@gmail.com>
2016-09-26 01:11:43 +0700

    Initial Commit

---
+++ Prework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Hong Ha Vo on 9/26/16.
+//  Copyright © 2016 Hong Ha Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
